11b8f84d8cc591e748350cee08eb3d3898ec20d5
Owen, Brian <brian@brianmowen.com>
2022-03-09 09:24:53 -0500

    add multiline string support

--- input/src/main/scala/fix/UnusedStringInterpolation.scala
+++ input/src/main/scala/fix/UnusedStringInterpolation.scala
@@ -6,4 +6,9 @@ package fix
 object UnusedStringInterpolation {
   val foo = s"bar"
   val uri = f"http://localhost"
+  val multiline =
+    s"""foo
+        bar
+        baz
+       """
 }
--- output/src/main/scala/fix/UnusedStringInterpolation.scala
+++ output/src/main/scala/fix/UnusedStringInterpolation.scala
@@ -3,4 +3,9 @@ package fix
 object UnusedStringInterpolation {
   val foo = "bar"
   val uri = f"http://localhost"
+  val multiline =
+    """foo
+        bar
+        baz
+       """
 }
