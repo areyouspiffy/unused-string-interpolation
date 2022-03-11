909c2af60796f01b45c9bb9df873d2db6e24089d
Brian Owen <brian.owen@banno.com>
2022-03-11 08:58:47 -0500

    add multiline string support (#3)

--- rules/src/main/scala/fix/UnusedStringInterpolation.scala
+++ rules/src/main/scala/fix/UnusedStringInterpolation.scala
@@ -6,9 +6,15 @@ import scala.meta._
 class UnusedStringInterpolation extends SemanticRule("UnusedStringInterpolation") {
 
   override def fix(implicit doc: SemanticDocument): Patch = {
-    doc.tree.collect { case v @ Term.Interpolate(Name("s"), Lit.String(s) :: Nil, Nil) =>
-      Patch.replaceTree(v, s""""$s"""")
-    }.foldLeft(Patch.empty)(_ + _)
+    doc.tree
+      .collect { case v @ Term.Interpolate(Name("s"), lit @ Lit.String(s) :: Nil, Nil) =>
+        (lit.head.pos.start - (v.pos.start + 1)) match {
+          case 1 => Patch.replaceTree(v, """"""" + s + """"""")
+          case 3 => Patch.replaceTree(v, """"""""" + s + """"""""")
+          case _ => Patch.empty
+        }
+      }
+      .foldLeft(Patch.empty)(_ + _)
   }
 
 }
